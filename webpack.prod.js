685b009b12b8780b8f1318c3717d354789ddbe43
github username <github email address>
2021-03-21 16:17:21 +0000

    changed config to inject all js into html template

--- webpack.prod.js
+++ webpack.prod.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+      inject: true,
     }),
     new MonacoWebpackPlugin({
       languages: ["json", "javascript", "cpp", "ruby"],
